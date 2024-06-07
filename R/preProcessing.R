f1394b38a31375b620b5fca775e00f969ce89b19
Antonio De Falco <antoniodf92na@gmail.com>
2024-06-07 10:45:28 +0200

    Update preProcessing.R
    Fix_Dup

--- R/preProcessing.R
+++ R/preProcessing.R
@@ -29,8 +29,13 @@ annotateGenes <- function(mtx, organism = "human"){
   genes_inters <- intersect(rownames(mtx), edb[[use_geneID]])
   mtx <- mtx[which(rownames(mtx) %in% genes_inters),]
   edb <- edb[which(as.vector(edb[[use_geneID]]) %in% genes_inters),]
-  edb <- edb[!duplicated(edb$gene_name),]
   edb <- edb[order(match(edb[[use_geneID]], rownames(mtx))),]
+  
+  Dupl_GeneName <- duplicated(edb$gene_name)
+  edb <- edb[!Dupl_GeneName,]
+  mtx <- mtx[!Dupl_GeneName,]
+
+  
   if(class(mtx)[1]=="dgCMatrix"){
     mtx_annot <- cbind(edb, as.matrix(mtx))
   }else{
